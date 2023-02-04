444affb6841a5d2d20cd37eb4941408737127b1f
Emilio <emiliorr2013@gmail.com>
2023-02-04 13:29:03 -0500

    Ejmplo modulo 11

---
+++ Modulo11/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtClave = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtClaveInvertida = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDigitoVerificador = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtClaveSri = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtClaveSri)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtDigitoVerificador)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtClaveInvertida)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtClave)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 262)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "MODULO 11"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(312, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(210, 61)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "GENERAR DIGITO VERIFICADOR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtClave
+        '
+        Me.txtClave.Location = New System.Drawing.Point(19, 48)
+        Me.txtClave.Name = "txtClave"
+        Me.txtClave.Size = New System.Drawing.Size(503, 22)
+        Me.txtClave.TabIndex = 1
+        Me.txtClave.Text = "031220140117223322590011000000000000000000000352"
+        Me.txtClave.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "CLAVE:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "CLAVE INVERTIDA:"
+        '
+        'txtClaveInvertida
+        '
+        Me.txtClaveInvertida.Location = New System.Drawing.Point(19, 102)
+        Me.txtClaveInvertida.Name = "txtClaveInvertida"
+        Me.txtClaveInvertida.Size = New System.Drawing.Size(503, 22)
+        Me.txtClaveInvertida.TabIndex = 3
+        Me.txtClaveInvertida.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 151)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(154, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "DIGITO VERIFICADOR:"
+        '
+        'txtDigitoVerificador
+        '
+        Me.txtDigitoVerificador.Location = New System.Drawing.Point(19, 171)
+        Me.txtDigitoVerificador.Name = "txtDigitoVerificador"
+        Me.txtDigitoVerificador.Size = New System.Drawing.Size(147, 22)
+        Me.txtDigitoVerificador.TabIndex = 5
+        Me.txtDigitoVerificador.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 196)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "CLAVE SRI:"
+        '
+        'txtClaveSri
+        '
+        Me.txtClaveSri.Location = New System.Drawing.Point(19, 216)
+        Me.txtClaveSri.Name = "txtClaveSri"
+        Me.txtClaveSri.Size = New System.Drawing.Size(503, 22)
+        Me.txtClaveSri.TabIndex = 7
+        Me.txtClaveSri.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(575, 305)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "SRI - ECUADOR - MODULO 11"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtClave As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtClaveSri As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtDigitoVerificador As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtClaveInvertida As TextBox
+End Class
---
+++ Modulo11/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    'Este algoritmo lo puedes aplicar a una cadena de n caracteres para generar el digito verificador de dicha cedena
+    'Acorde a Ficha técnica, Manual de usuario, catálogo y especificaciones técnicas, Método de automatización off-line.
+    'ACTUALIZADO ENERO 2022. Versión 2.21
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'lineaS para comprobar la funcion
+        'VALOR DE EJEMPLO = 031220140117223322590011000000000000000000000352 -> el resultado debe ser = 9
+        'VELOR DE EJMPLO MANUAL SRI: 41261533 ->  el resultado debe ser = 6
+        txtDigitoVerificador.Text = getDigitoComprobador(txtClave.Text.Trim)
+    End Sub
+    Public Function getDigitoComprobador(clave)
+        Try
+            'ALMACENA EL DIGITO VERIFICADOR
+            Dim digito_verificador As String
+            'CONTROLA EL MAXIMO MULTIPLICADOR
+            Dim MAX As Int64 = 7
+            'CONTROLA EL MINIMO MULTIPLICADOR -> LUEGO SE INCREMENTARA HASTA IGUALAR A MAX
+            Dim MIN As Int64 = 2
+            'PARA FACILITAR EL RECORRIDO INVIERTO LA CLAVE
+            Dim claveInvertida As String = Microsoft.VisualBasic.StrReverse(clave)
+            txtClaveInvertida.Text = claveInvertida
+
+            Dim resultado As Int64 = 0
+            Dim resultado_acumulado As Int64 = 0
+            For n = 0 To Len(claveInvertida) - 1
+                'OBTENGO EL VALOR (n) DE LA CLAVE INVERTIDA
+                Dim valor As String = claveInvertida.Chars(n)
+                'RESULTADO ALMACENA EL VALOR OBTENIDO POR EL MINIMO MULTIPLICADOR
+                resultado = Convert.ToInt64(valor) * MIN
+                'SE ACUMULA EL RESULTADO 
+                resultado_acumulado = resultado_acumulado + resultado
+                'CONTROLA EL MULTIPLICADOR
+                If (MIN >= MAX) Then
+                    'REINICIAMOS EL MINIMO MULTIPLICADOR SI YA SE ALCANZO EL MAXIMO
+                    MIN = 2
+                Else
+                    'INCREMENTAMOS EL MINIMO MULTIPLICADOR
+                    MIN = MIN + 1
+                End If
+                'REPETIMOS EL PROCESO
+            Next n
+            ' MsgBox("resultado: " + Convert.ToString(resultado_acumulado))
+            digito_verificador = 11 - (resultado_acumulado Mod 11)
+            If digito_verificador = 10 Then
+                digito_verificador = 1
+            ElseIf digito_verificador = 11 Then
+                digito_verificador = 0
+            End If
+            txtClaveSri.Text = clave + digito_verificador
+            Return digito_verificador
+        Catch ex As Exception
+            MsgBox("error al generar digito comprobador " + ex.Message, MsgBoxStyle.Information)
+        End Try
+    End Function
+End Class
